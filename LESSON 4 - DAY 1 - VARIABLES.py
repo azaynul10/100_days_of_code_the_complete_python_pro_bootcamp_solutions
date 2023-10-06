403d2f63625c3bde3f710bbe897136075d62f82c
Zaynul Abedin Miah <111334392+azaynul10@users.noreply.github.com>
2023-10-06 17:24:32 +0600

    Create LESSON 4 - DAY 1 - VARIABLES.py

---
+++ LESSON 4 - DAY 1 - VARIABLES.py
@@ -0,0 +1,12 @@
+# There are two variables, a and b from input
+a = input()
+b = input()
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+a,b = b,a
+
+
+# Write your code above this line ☝️
+print("a: " + a)
+print("b: " + b)

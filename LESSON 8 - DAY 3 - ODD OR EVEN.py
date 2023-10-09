8e64372052f86fa1bb83aca0566f41b3bb7ea235
Zaynul Abedin Miah <111334392+azaynul10@users.noreply.github.com>
2023-10-09 17:43:17 +0600

    Create LESSON 8 - DAY 3 - ODD OR EVEN.py

---
+++ LESSON 8 - DAY 3 - ODD OR EVEN.py
@@ -0,0 +1,8 @@
+# Which number do you want to check?
+number = int(input())
+# 🚨 Don't change the code above 👆
+if number%2==0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
+# Write your code below this line 👇
